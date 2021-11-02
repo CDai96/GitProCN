26ef7f0f0765fdcd22c6e2c7cb9b7d7beecf2908
Ziyao Zhu <Ziyao_Zhu@epam.com>
2021-11-02 16:05:11 +0800

    b3

--- src/Cheerer.java
+++ src/Cheerer.java
@@ -1,6 +1,6 @@
 public class Cheerer {
     public static void cheer(){
-        System.out.println("We are Epam Family");
+        System.out.println("We all love java");
     }
 
     public static void main(String[] args) {
