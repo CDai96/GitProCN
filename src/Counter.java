f0bc1e3958cb23358d589ff4464d3a00dd860884
GarrisonC <garrison123@126.com>
2021-11-02 17:09:04 +0800

    c6

--- src/Counter.java
+++ src/Counter.java
@@ -1,11 +1,12 @@
 public class Counter {
 
     public static void main(String[] args) {
-        printAge(3, 17);
+        printAge(3, 17,999);
     }
 
-    public static void printAge(int firstInt, int secondInt) {
+    public static void printAge(int firstInt, int secondInt, int thirdInt) {
         System.out.println(firstInt);
         System.out.println(secondInt);
+        System.out.println("Counting the countless" + thirdInt);
     }
 }
