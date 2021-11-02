a919d5b5ef3560c7f0749096ddfbf6743bbeff52
Cina Liu <cina_liu@epam.com>
2021-11-02 17:05:56 +0800

    d6

--- src/Cheerer.java
+++ src/Cheerer.java
@@ -1,6 +1,6 @@
 public class Cheerer {
     public static void cheer(String teamName){
-        System.out.println("We are " + teamName);
+        System.out.println("Cheer for the  " + teamName);
     }
 
     public static void main(String[] args) {
