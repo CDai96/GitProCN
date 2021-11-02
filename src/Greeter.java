18061cf5a567050546712c4b513ccb7b235111bf
GarrisonC <garrison123@126.com>
2021-11-02 17:01:32 +0800

    c5

--- src/Greeter.java
+++ src/Greeter.java
@@ -1,10 +1,10 @@
 public class Greeter {
     public static void greeter(String name){
-<<<<<<< HEAD
+
         System.out.println("Greetings ");
-=======
+
         System.out.println("Greetings " + name);
->>>>>>> 65f1070f12f21835353ac6cfdb0727b6c9c7022a
+
     }
 
     public static void main(String[] args) {
