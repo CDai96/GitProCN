4e940ccd39e3417ac116fbfbd42102aba0acdab9
Garrison Chen <Garrison_Chen@epam.com>
2021-11-02 15:37:38 +0800

    c1

---
+++ src/Cheerer.java
@@ -0,0 +1,9 @@
+public class Cheerer {
+    public static void cheer(){
+        System.out.println("We are Epam Family");
+    }
+
+    public static void main(String[] args) {
+        cheer();
+    }
+}
