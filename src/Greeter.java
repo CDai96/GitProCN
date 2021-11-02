258b619cd75a55ba6a8aa5b9060aac51db434293
Mike Dai <Mike_Dai@epam.com>
2021-11-02 16:54:42 +0800

    a5

--- src/Greeter.java
+++ src/Greeter.java
@@ -1,13 +1,9 @@
 public class Greeter {
-    public static void greeter(String name){
-<<<<<<< HEAD
-        System.out.println("Greetings ");
-=======
-        System.out.println("Greetings " + name);
->>>>>>> 65f1070f12f21835353ac6cfdb0727b6c9c7022a
+    public static void greeter(String name, String greetWord){
+        System.out.println(greetWord +  "! " + name);
     }
 
     public static void main(String[] args) {
-        greeter("Mike");
+        greeter("Mike", "Nihao");
     }
 }
