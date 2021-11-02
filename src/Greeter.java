6ae82ad6cc8ec5eb12daa761752883ee250a9355
Ziyao Zhu <Ziyao_Zhu@epam.com>
2021-11-02 17:06:53 +0800

    b6

--- src/Greeter.java
+++ src/Greeter.java
@@ -1,9 +1,10 @@
 public class Greeter {
     public static void greeter(String name){
-        System.out.println("Greetings " + name);
+        System.out.println("Greetings for the " + name);
     }
 
     public static void main(String[] args) {
         greeter("Mike");
     }
 }
+git
