05d64ced5bf3acd3d3cd22f3c95db678a4b1eec6
Cina Liu <cina_liu@epam.com>
2021-11-02 16:21:30 +0800

    d3

--- src/Greeter.java
+++ src/Greeter.java
@@ -1,6 +1,6 @@
 public class Greeter {
     public static void greeter(String name){
-        System.out.println("Hello " + name);
+        System.out.println("Greetings");
     }
 
     public static void main(String[] args) {
